a17ce2696680f15826899f88b84a06fa5b4a1993
jahed-12 <hosenmahmud398@gmail.com>
2025-08-01 17:08:35 +0600

    Add files via upload

---
+++ ok.py
@@ -0,0 +1,109 @@
+import os
+import sys
+import time
+os.system('clear')
+
+print('\n\n')
+jahed = "   Please Wait Dear..."
+
+for jh in jahed:
+	sys.stdout.write(jh)
+	sys.stdout.flush()
+	time.sleep(0.2)
+time.sleep(2)
+print('\n\n\n\n')
+os.system('clear')
+
+print('\n\n\n\n')
+
+jahed = "\x1b[1;33m       System Loading..."
+
+for jh in jahed:
+	sys.stdout.write(jh)
+	sys.stdout.flush()
+	time.sleep(0.2)
+time.sleep(2)
+print('\n\n\n\n\n\n')
+os.system('clear')
+
+time.sleep(0.10)	
+
+print('\n\n\n\n\n\n\n')
+
+jahed = "\x1b[38;5;208m               System Loaded Successfully...!!"
+
+for jh in jahed:
+	sys.stdout.write(jh)
+	sys.stdout.flush()
+	time.sleep(0.2)
+print('\n')
+nm = input("\x1b[38;5;160m               Enter Your Name : ")
+print('\n')
+jahed = "\x1b[38;5;46m Assalamu Alaikum % Wellcome To My Tool Dear:- "+nm
+
+for jh in jahed:
+	sys.stdout.write(jh)
+	sys.stdout.flush()
+	time.sleep(0.2)
+time.sleep(3.0)
+
+print('\n')
+
+os.system('clear')
+
+print("""
+
+_  _ ____ _    _  _ _ _  _ ____ 
+|  | |__| |    |_/  | |\ | | __ 
+ \/  |  | |    | \_ | | \| |__]
+\x1b[1;34m_____________________________________
+Owner : Jahedul Islam
+Facebook Page : Md Jahed
+Tool : Free
+Version : 2.0
+\x1b[1;34m_____________________________________\033[0m  """)
+
+
+print('\033[1;30m')
+os.system("apt update && apt upgrade")
+print('\033[1;31m')
+os.system("pkg update")
+print('\033[1;32m')
+os.system("pkg upgrade")
+print('\033[1;33m')
+os.system("pkg install python")
+print('\033[1;34m')
+os.system("pkg install python2")
+print('\033[1;35m')
+os.system("pkg install python3")
+print('\033[1;36m')
+os.system("pkg install macchanger")
+print('\033[1;37m')
+os.system("pkg install java")
+print('\033[1;38m')
+os.system("pkg install nmap")
+print('\033[1;39m')
+os.system("pkg install bash")
+print('\033[1;40m')
+os.system("pkg install nano")
+print('\033[1;41m')
+os.system("pkg install curl")
+print('\033[1;42m')
+os.system("pip install requests")
+print('\033[1;43m')
+os.system("pip install httpx")
+print('\033[1;44m')
+os.system("pip install rich")
+print('\033[1;45m')
+os.system("pip install bs4")
+print('\033[0m')
+
+print("🆃🅴🆁🅼🆄🆇 🆂🅴🆃🆄🅿 🅳🅾🅽🅴")
+
+
+
+
+
+
+
+
